6b1070a30029aec8f483455b16692352dd1f92c8
Jason Soo <jasonsoo@Jasons-MacBook-Air-51.local>
2024-02-25 19:20:35 -0800

    init push

---
+++ src/main/java/com/gtg/gtg/GtgApplication.java
@@ -0,0 +1,13 @@
+package com.gtg.gtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gtg/gtg/GtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtg.gtg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
