d858e0bc96ebfa3377a504c915fca8f69cfc7912
ltanafranca1004 <luistanafranca@gmail.com>
2024-03-05 17:07:12 -0800

    updated login feature

--- src/main/java/com/gtg/gtg/controllers/UsersController.java
+++ src/main/java/com/gtg/gtg/controllers/UsersController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
 
 import com.gtg.gtg.models.Users;
 import com.gtg.gtg.models.UsersRepository;
 
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Map;
+import java.sql.Date;
 
 @Controller
 public class UsersController {
@@ -24,4 +29,42 @@ public class UsersController {
         return "main/login";
     }
     
+    @PostMapping("/users/add")
+    public String addUser(@RequestParam Map<String, String> newUser, HttpServletResponse response) {
+        // Extract user attributes from the request parameters
+        String username = newUser.get("username");
+        String name = newUser.get("name");
+        String email = newUser.get("email");
+        String password = newUser.get("password");
+        Date birthday = newUser.get("birthday") != null ? Date.valueOf(newUser.get("birthday")) : null;
+    
+        // Create and save the new user entity
+        Users user = new Users(username, name, email, password, birthday);
+        UsersRepo.save(user);
+    
+        // Set the response status code to 201 Created
+        response.setStatus(HttpServletResponse.SC_CREATED);
+    
+        // Redirect to a view, e.g., to list all users or to a user profile page
+        return "main/login"; // Adjust the redirect as necessary for your application
+    }
+    
+    @PostMapping("/login")
+    public String processLogin(@RequestParam Map<String, String> User, HttpServletResponse response) {
+
+        System.out.println("meow");
+
+        List<Users> getUser = UsersRepo.findByUsernameAndPassword( User.get("name"), User.get("password"));
+
+        if (!getUser.isEmpty()){
+
+            response.setStatus(200);
+            return "test.html";
+
+        } else {
+            return "test.html";
+        }
+        
+    }   
+
 }
--- src/main/java/com/gtg/gtg/models/UsersRepository.java
+++ src/main/java/com/gtg/gtg/models/UsersRepository.java
@@ -1,8 +1,8 @@
 package com.gtg.gtg.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UsersRepository extends JpaRepository<Users,Integer> {
-
-    
+    List<Users> findByUsernameAndPassword(String username, String password);    
 }
