13661b88093160456991d275c343d645264a67c8
Jason Soo <jasonsoo@d207-023-213-198.wireless.sfu.ca>
2024-03-27 14:41:01 -0700

    init Tests

---
+++ src/main/java/com/gtg/gtg/TestingWebApplication.java
@@ -0,0 +1,11 @@
+package com.gtg.gtg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingWebApplication {
+    public static void main(String[] args) {
+		SpringApplication.run(TestingWebApplication.class, args);
+	}
+}
